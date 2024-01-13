2636d340ca082a556ffaccd88c13a1a1cd3cbe01
Aptura <aptura@users.noreply.github.com>
2024-01-13 19:11:21 +0100

    misc: try to fix but not working for now

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,19 +1,22 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionsBitField,
+} = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("unban")
     .setDescription("Cette commande permet de unban un utilisateur du serveur.")
-    .addUserOption((option) =>
+    .addStringOption((option) =>
       option
         .setName("userid")
         .setDescription("Pseudo de l'utilisateur qu'il faut unban")
         .setRequired(true)
     ),
   async execute(interaction) {
-    const { channel, options } = interaction;
-    const unbanUser = interaction.options.getInteger("idutilisateur");
+    const { channel, options, guild } = interaction;
+    const unbanUser = options.getString("idutilisateur");
 
     if (
       !interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)
@@ -25,9 +28,9 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setColor("Red")
-      .setDescription(`\`✅\` L'ID: ${unbanUser} a été **unban**`);
+      .setDescription(`\`✅\` L'ID: ${unbanUser} a été **unban**.`);
 
-    await interaction.guild.members.unban(unbanUser).catch((err) => {
+    await guild.members.unban(unbanUser).catch((err) => {
       interaction.reply({ content: "Il y a eu une erreur", ephemeral: true });
     });
 
